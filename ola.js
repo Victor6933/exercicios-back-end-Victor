96d6944bd5af0ee1f819bec3e001711d9036555b
Victor6933 <cordeiro.lima.victor@escola.pr.gov.br>
2026-04-30 09:39:06 -0300

    Add files via upload

--- ola.js
+++ ola.js
@@ -1,1 +1,7 @@
 console.log("ola back");
+const os = require ('os');
+const nome = process.argv[2] ||"usuário";
+console.log(Olá, ${nome}! Bem-vindo ao Node.js);
+console.log(Versão do Node.js: $ {process.vesion});
+console.log(Sistema operacional: ${os.type()} ${os.releas()});
+console.log(Diretório atual: $ {process.cwd()}):
