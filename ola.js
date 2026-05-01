270c3e1ed208022581c1e88c5bebe37d9b337b12
Victor6933 <cordeiro.lima.victor@escola.pr.gov.br>
2026-04-30 21:49:17 -0300

    Add files via upload

--- ola.js
+++ ola.js
@@ -107,3 +107,73 @@ const top3 = alunos;
 
 console.log("\nTop 3 alunos da 3A");
 top3.forEach(a => console.log('${a.nome} - ${a.nota}'));
+
+const fs = require('fs');
+
+//catálogo inicial
+let catalogoFilmes = [
+{ id: 1, titulo: "Matrix", ano: 1999, diretor: "Lana Wachowski", genero: "Ficção", nota: 4.8 },
+{ id: 2, titulo: "Titanic", ano: 1997, diretor: "James Cameron", genero: "Romance", nota: 4.5 },
+{ id: 3, titulo: "Toy Story", ano: 1995, diretor: "John Lasseter", genero: "Animação", nota: 4.7 },
+{ id: 4, titulo: "O Poderoso Chefão", ano: 1972, diretor: "Francis Ford Coppola", genero: "Drama", nota: 4.9 },
+{ id: 5, titulo: "Star Wars", ano: 1977, diretor: "George Lucas", genero: "Ficção", nota: 4.6 }
+];
+
+//1.Converter para JSON 
+const jsonString = 
+JSON.stringify(catalogoFilmes, null, 2);
+console.log(jsonString);
+console.log("tipo>", typeof jsonString);
+
+//2.Simular JSON de API
+const novosFilmesJSON = '[
+{ "id": 6, "titulo": "Avatar", "ano": 2009, "diretor": "James Cameron", "genero": "Ficção", "nota": 4.4 },
+{ "id": 7, "titulo": "Vingadores", "ano": 2012, "diretor": "Joss Whedon", "genero": "Ação", "nota": 4.3 },
+{ "id": 8, "titulo": "Frozen", "ano": 2013, "diretor": "Chris Buck", "genero": "Animação", "nota": 4.2 }
+]';
+
+//converter JSON para objetivo
+const novosFilmes =
+JSON.parse(novosFilmesJSON);
+
+//Adicionar ao catálogo
+catalogoFilmes =
+catalogoFilmes.concat(novosFilmes);
+
+//3. Buscar por ID
+function buscarFilmes.find(id) {
+    return catalogoFilmes.find(f => f.id === id) || null;
+}
+
+console.log("\nBuscar ID 3:",
+    buscarFilmesporID(3));
+    console.log("Buscar ID 99:"
+        buscarFilmesporID(99));
+
+//4. Buscar por gênero
+function buscarporGênero(genero) {
+    return catalogoFilmes.filter(f => f.genero === genero);
+}
+
+console.log("\nFIlmes de Ficção:");
+console.log(buscarporGênero("Ficção"));
+
+//5. Salvar em arquivo
+fs.writeFileSync('filmes.json',
+    JSON.stringify(catalogoFilmes, null, 2));
+
+//ler arquivo
+const dados = fs.readFileSync('filmes.json',
+    'utf-8');
+    const catalogoLido = JSON,parse(dados);
+
+console.log("\nArquivo lido com sucesso: ");
+console.log(catalogoLido);
+
+//6. Strings formatadas
+const fichas = catalogoFilmes.map(f=>{
+    return'${f.titulo} (${f.ano}) - Dirigido por ${f.diretor}\nGêneroo: ${f.genero} | nota ${f.nota}/5.0;
+});
+
+console.log("\nFichas dos filmes:");
+fichas.forEach(f => console.log(f + "\n"));
